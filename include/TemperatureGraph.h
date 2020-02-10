60c901327d24b326dc3919ce2d1e86f355066d8a
ivykoko1 <ramon.sanchez68@gmail.com>
2020-02-10 15:11:00 +0100

    Code formatting

--- include/TemperatureGraph.h
+++ include/TemperatureGraph.h
@@ -11,32 +11,33 @@ class TemperatureGraph
 {
 private:
     uint8_t points[128];
+    uint8_t lastIndex = 0;
     uint16_t lowerBound = LOWER_BOUND;
     uint16_t upperBound = UPPER_BOUND;
+
     float lastTemperature;
     float minTemperature;
     float maxTemperature;
+
     bool isGraphTempUpdated = false;
-    uint8_t lastIndex = 0;
+
     void shiftArray();
 
     typedef struct
     {
         uint16_t lower_bound;
         uint16_t upper_bound;
-    }  boundsTuple;
-    
+    } boundsTuple;
+
 public:
     TemperatureGraph(uint16_t lower_bound = LOWER_BOUND, uint16_t upper_bound = UPPER_BOUND);
     TemperatureGraph();
     ~TemperatureGraph();
-    void drawGraph(Adafruit_SSD1306* display);
+    void drawGraph(Adafruit_SSD1306 *display);
     float addTemperature(float temp);
     void reset();
     boundsTuple getGraphBounds();
     boundsTuple setGraphBounds();
 };
-
-
 
 #endif // !#TEMPGRAPH_H
--- src/TemperatureGraph.cpp
+++ src/TemperatureGraph.cpp
@@ -1,59 +1,67 @@
 #include <TemperatureGraph.h>
 
-
 TemperatureGraph::TemperatureGraph(/* args */)
 {
-    for(uint8_t x = 0; x < 128; x ++){
+    for (uint8_t x = 0; x < 128; x++)
+    {
         points[x] = -1;
-    }  
+    }
 }
 
-void TemperatureGraph::shiftArray(){
+void TemperatureGraph::shiftArray()
+{
     for (uint8_t i = 0; i < 127; i++)
     {
-        if(i < 127){
-            
+        if (i < 127)
+        {
+
             points[i + 1] = points[i];
             //TODO ADD DEBUG PRINTS
-            
-        }   
+        }
     }
-    
 }
 
 //TODO fix overflow, add dynamic bounds, display bounds (maybe on button press)
 
-void TemperatureGraph::drawGraph(Adafruit_SSD1306* display){
+void TemperatureGraph::drawGraph(Adafruit_SSD1306 *display)
+{
     display->clearDisplay();
     for (uint8_t i = 0; i < 128; i++)
     {
-        if(points[i] > -1){
+        if (points[i] > -1)
+        {
             display->drawPixel(i, points[i], SSD1306_WHITE);
         }
     }
     if (!isGraphTempUpdated)
     {
-        display->setCursor(90,25);
+        display->setCursor(90, 25);
         display->setTextColor(WHITE);
         display->print(lastTemperature);
-        display->print("C");    
+        display->print("C");
     }
-    
+
     display->display();
 }
 //todo check for min and max temps when adding.
 
-float TemperatureGraph::addTemperature(float temp){
-    if(temp == lastTemperature){
+float TemperatureGraph::addTemperature(float temp)
+{
+    if (temp == lastTemperature)
+    {
         isGraphTempUpdated = true;
-    }else{
+    }
+    else
+    {
         isGraphTempUpdated = false;
     }
     uint8_t coord = map((temp * 100), 1850, 2175, 32, 0);
-    if(lastIndex == 127){
+    if (lastIndex == 127)
+    {
         shiftArray();
         points[lastIndex] = coord;
-    }else
+    }
+    else
     {
         points[lastIndex] = coord;
         lastIndex++;
@@ -61,7 +69,6 @@ float TemperatureGraph::addTemperature(float temp){
     lastTemperature = temp;
     return temp;
 }
-
 
 TemperatureGraph::~TemperatureGraph()
 {
